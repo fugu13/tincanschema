31824b336d26bb4fc053ad6a267d4bc70c56a3da
Russell Duhon <fugu13@gmail.com>
2012-10-25 19:55:08 -0700

    first commit

---
+++ validate.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+var JSV = require('JSV').JSV;
+
+var allSchema = JSON.parse(fs.readFileSync('tincan.schema.json', 'utf8'));
+
+var json = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+var env = JSV.createEnvironment();
+
+var schema = env.createSchema(allSchema, null, 'tcapi:special');
+
+
+//validate a single statement or list of statements
+var report = env.validate(json, {"$ref": "tcapi:special#statementpost"});
+
+//TODO: validate length of mbox_sha1sum
+//TODO: language map keys -- really funky rules!
+//TODO: objectType hack for statement/substatement improvement?
+//TODO: duration validation
+//TODO: timestamp/stored validation
+//TODO: find out if there is a better way to handle the no additional agent properties thing
+//TODO: find out why minItems and maxItems aren't validating
+
+
+
+console.log("Done validating!");
+if (report.errors.length > 0) {
+    console.log("Errors!");
+    console.log(report.errors);
+} else {
+    console.log("No errors!");
+}
